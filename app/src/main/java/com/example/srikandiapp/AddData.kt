b19b87458d945357e12342ede2e67985d5a08e3c
Actanzil <tanziluadjiganesha@gmail.com>
2022-12-19 22:58:08 +0700

    Membuat Activity Baru

---
+++ app/src/main/java/com/example/srikandiapp/AddData.kt
@@ -0,0 +1,11 @@
+package com.example.srikandiapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddData : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_data)
+    }
+}
