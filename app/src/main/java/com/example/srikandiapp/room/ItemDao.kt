7ce1c9b9af12ef8eb92ded1320efbd0014455b6f
Actanzil <tanziluadjiganesha@gmail.com>
2022-12-19 22:55:30 +0700

    Menambah Fungsi Tambah Data

--- app/src/main/java/com/example/srikandiapp/room/ItemDao.kt
+++ app/src/main/java/com/example/srikandiapp/room/ItemDao.kt
@@ -1,10 +1,15 @@
 package com.example.srikandiapp.room
 
 import androidx.room.Dao
+import androidx.room.Insert
 import androidx.room.Query
 
 @Dao
 interface ItemDao {
+
+    @Insert
+    fun addDataItem(item: Item)
+
     @Query("SELECT * FROM item ORDER BY nama ASC")
     fun getDataItem() : List<Item>
 
